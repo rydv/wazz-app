47ac062c346501a253110e90b7589d1ddaa2b0da
Sanjay <sanjayrydv@gmail.com>
2023-02-03 01:28:53 +0530

    Desing part completed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Message = () => {
+export const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0bfYiqpFmDkRpg-B2tspMCkLpQrz9gt9egw&usqp=CAU" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/Charanya-Kannan.jpg?itok=vSbk0no5" alt="" />
+      </div>
+    </div>
   )
 }
-
-export default Message
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ export const Search = () => {
   return (
     <div className="search">
       <div className="searchForm">
-      <input type="text" placeholder='search a user'/>
+      <input type="text" placeholder='Find user'/>
       </div>
       <div className="userChat">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Napw0Q6qX184KpJrT5oNxD9b8lFJ2q2jsovOPlE3emHay5vJEM1GeCQTRX9AfiLOw8s&usqp=CAU" alt="" />
