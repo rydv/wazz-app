3ac172762e076b1d15c33f90d8aa389526a4ca36
Sanjay <sanjayrydv@gmail.com>
2023-03-03 04:43:29 +0530

    Final working

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -51,27 +51,37 @@ export const Input = () => {
     await updateDoc(doc(db,"userChats",currentUser.uid),{
       [data.chatId+".lastMessage"]:{text},
       [data.chatId+".date"]: serverTimestamp()
-    })
+    });
 
     await updateDoc(doc(db,"userChats",data.user.uid),{
       [data.chatId+".lastMessage"]:{text},
       [data.chatId+".date"]: serverTimestamp()
-    })
+    });
 
-    setText('')
-    setImg(null)
+    setText('');
+    setImg(null);
   }
   return (
     <div className="input">
-        <input type="text" placeholder='Type something...' onChange={e => setText(e.target.value)}/>
-        <div className="send">
-            <img src={Attach} alt="" />
-            <input type="file" style={{display: "none"}} id='file' onChange={e => setImg(e.target.files[0])}/>
-            <label htmlFor="file">
-                <img src={Img} alt="" />
-            </label>
-            <button onClick={handleSend}>Send</button>
-        </div>
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button onClick={handleSend}>Send</button>
+      </div>
     </div>
   )
 }
