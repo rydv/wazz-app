85513ec7d4a7e4f4f44ad9385f70976ee22aa64e
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2023-03-03 03:06:07 +0530

    not working model

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { ChatContext } from '../context/ChatContext'
 import {Message} from './Message'
+import {db} from '../firebase';
+import {doc, onSnapshot} from 'firebase/firestore';
 
 export const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const {data} = useContext(ChatContext);
+
+  useEffect(
+    () => {
+      const unSub = onSnapshot(doc(db,"chats", data.chatId), (doc)=>{
+        doc.exists() && setMessages(doc.data().messages)
+      })
+
+      return () => {
+        unSub()
+      }
+    },[data.chatId]
+  )
+  
   return (
     <div className="messages">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+
+      {messages.map(m => (
+        <Message message={m} key={m.id}/>
+      ))}
     </div>
   )
 }
